637f3505602386446250aa8b713039646ab8dc20
Aaron Luna <aaron.yamil@gmail.com>
2023-05-05 13:32:54 -0400

    feat: add RTL and Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+export default {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+}
