e8b5e39311dd550900e42d913035cbd0cbfd30b1
Aaron Luna <aaron.yamil@gmail.com>
2023-05-06 15:38:18 -0400

    Feat: add footer

---
+++ src/components/Footer.jsx/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+        <span>@ Google 2021</span>
+        <span>version 0.1.0</span>
+    </footer>
+  )
+}
