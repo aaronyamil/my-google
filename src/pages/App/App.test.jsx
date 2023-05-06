67f8ce53b623cd5e8de135105b0337f1463c9306
Aaron Luna <aaron.yamil@gmail.com>
2023-05-05 20:41:51 -0400

    fix: RTL with Vite

--- src/pages/App/App.test.jsx
+++ src/pages/App/App.test.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 describe('App tests', () => {
     it('should contains the heading 1', () => {
     render(<App />);
-        const heading = screen.getByText(/Agile Content Frontend Test/i);
+        const heading = screen.getByText(/Frontend Test/i);
         expect(heading).toBeInTheDocument()
     });
 });
