3264b5cfb02aa161fe3700e1e1a90ff7e52d2023
Aaron Luna <aaron.yamil@gmail.com>
2023-05-07 15:28:43 -0400

    feat: add skeleton loading

---
+++ src/components/Skeleton/index.jsx
@@ -0,0 +1,12 @@
+import './skeleton.css'
+const Skeleton = () => {
+  return (
+    <div className="skeleton-blog">
+        <div className="skeleton-title"></div>
+        <div className="skeleton-body"></div>
+        <div className="skeleton-footer"></div>
+    </div>
+  );
+};
+
+export default Skeleton;
