19d323ca0223488167c3a4539fc2f61085312c03
Aaron Luna <aaron.yamil@gmail.com>
2023-05-07 21:32:29 -0400

    feat: Card component

---
+++ src/components/CardData/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./CardData.css";
+
+export const CardData = ({ item, setItemSelected }) => {
+  return (
+    <div className="card-container">
+      <button className="close-btn" onClick={()=>setItemSelected(null)}>
+        X
+      </button>
+      <img src={item.strMealThumb} alt={item.strMeal} className="card-img" />
+      <div className="search-detail-link">
+        <a href={item?.strYoutube} target="_blank" className="search-a">
+          {item?.strYoutube}
+        </a>
+      </div>
+      <div className="card-detail-name">{item?.strMeal}</div>
+      <div className="card-detail-detail">{item?.strInstructions}</div>
+    </div>
+  );
+};
