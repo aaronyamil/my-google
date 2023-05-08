13e7f4d35acb745a6aef99e98e63cb4f27cf5978
Aaron Luna <aaron.yamil@gmail.com>
2023-05-07 22:02:11 -0400

    feat: add app test

--- src/pages/App/App.test.jsx
+++ src/pages/App/App.test.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import App from './App.jsx';
 
 describe('App tests', () => {
-    it('should contains the heading 1', () => {
+    it('should contains the header, home and footer', () => {
     render(<App />);
         const heading = screen.getByText(/Frontend Test/i);
-        expect(heading).toBeInTheDocument()
+        expect(heading).toBeInTheDocument();
+        expect(screen.getByRole('button')).toBeDisabled();
+        const footer = screen.getByText(/@ Google 2021/i);
+        expect(footer).toBeInTheDocument();
     });
 });
